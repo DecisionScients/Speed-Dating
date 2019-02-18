1ebc0bfc4e4ce35ce42e35e02349e5396d2ee11a
John James <jjames@decisionscients.com>
2019-02-18 12:56:50 -0500

    Initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis, Modeling and Prediction of Mate Selection and Matching ',
+    author='John James',
+    license='MIT',
+)
